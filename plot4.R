c16773667325b9412b8d507056bca30bfe2de286
karthikchawala <30761484+karthikchawala@users.noreply.github.com>
2017-08-24 21:52:35 -0500

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,33 @@
+
+# reading the data
+data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+
+
+# subsetting the data
+data1 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007"))
+
+
+
+#converting dates
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+datetime <- paste(as.Date(data1$Date), data1$Time)
+data1$Datetime <- as.POSIXct(datetime)
+
+# plotting the data
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(data1, {
+  plot(Global_active_power ~ Datetime, type="l", ylab="Global Active Power", xlab="")
+  plot(Voltage ~ Datetime, type="l", ylab="Voltage", xlab="datetime")
+  plot(Sub_metering_1 ~ Datetime, type="l", ylab="Energy sub metering", xlab="")
+  lines(Sub_metering_2 ~ Datetime,col='Red')
+  lines(Sub_metering_3 ~ Datetime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power ~ Datetime, type="l", ylab="Global_reactive_power",xlab="datetime")
+})
+
+
+
+#creating png file
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
