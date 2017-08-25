aa23067cb0d7ec89eb5f48a3596456485a080bf9
karthikchawala <30761484+karthikchawala@users.noreply.github.com>
2017-08-24 21:51:06 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,23 @@
+
+# reading the data
+data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+
+
+# subsetting the data
+data1 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007"))
+
+
+
+#converting dates
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+
+
+# plotting the data
+hist(data1$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+
+
+#creating png file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
